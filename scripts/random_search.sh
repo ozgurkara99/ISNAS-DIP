a1fbc1997fade67099ac49e388e59f15ce8cb841
Ozgur Kara <ozgurrkara99@gmail.com>
2022-05-22 22:16:38 +0200

    cluster scripts

---
+++ scripts/random_search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH  --output=sbatch_log/%j.out
+#SBATCH  --gres=gpu:1
+#SBATCH  --mem=30G
+
+
+
+eval "$(/itet-stor/aricanm/net_scratch/conda/bin/conda shell.bash hook)"
+
+echo 'activating conda environment'
+conda activate pytcu10
+
+echo "args: $@"
+python -u '/srv/beegfs02/scratch/biwismrschool21/data/NAS-DIP Summer Research/random_search.py' "$@"
+
+
